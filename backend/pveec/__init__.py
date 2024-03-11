923331ad099869aecb880ba9a72fd46f57ff6e4c
WytheHuang <zxc22143579.wythe@gmail.com>
2024-03-11 14:45:08 +0800

    delete unused app

--- backend/pveec/__init__.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class PveecConfig(AppConfig):
-    """PVEEC application configuration."""
-
-    name = "pveec"
-    verbose_name = "PVEEC"
--- backend/pveec/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class PveecConfig(AppConfig):
-    """AppConfig for pveec."""
-
-    name = "pveec"
-    auto_field = "django.db.models.BigAutoField"
